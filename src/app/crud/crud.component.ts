d0df5c4abc19765ac84e32b135f6bdaf416401c6
rodrigofries01 <rfries24@gmail.com>
2024-09-26 15:54:58 -0300

    feat: form & table created

---
+++ src/app/crud/crud.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud.component.html',
+  styleUrl: './crud.component.css'
+})
+export class CrudComponent {
+  alunos: string[] = ['Otis', 'Aurora'];
+
+}
