f309cb3b595b543bd37aeb754b3a223115d3fb04
rodrigofries01 <rfries24@gmail.com>
2024-11-08 12:24:00 -0300

    return

--- src/app/pipes/media-aluno.pipe.ts
+++ src/app/pipes/media-aluno.pipe.ts
@@ -3,15 +3,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'mediaAluno',
-  standalone: true
+  standalone: true,
 })
 export class MediaAlunoPipe implements PipeTransform {
-  transform(media: Aluno[], buscarAluno: string): Aluno[] {
-    let mediaAluno = (media. + media.nota2) / 2;
-    if (!media || !buscarAluno) {
-      return mediaAluno;
-    }
+  transform(media: any): number {
+    return (media.nota1 + media.nota2) / 2;
   }
-
 }
 // TODO: Prosseguir com correção desse erro, nota1/2 não esta declarados. instalação de pacotes do projeto
